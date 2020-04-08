769ba183d25522b18dad05669b700e54faccfad1
Martin Heidegger <martin.heidegger@gmail.com>
2020-04-08 20:20:56 +0900

    chore: removed esm dependency

--- website/get-readme.mjs
+++ website/get-readme.mjs
@@ -1,9 +1,11 @@
-import { markdowner } from 'smh-markdowner'
+import smhMarkdowner from 'smh-markdowner'
 import { promises as fs } from 'fs'
 import path from 'path'
 
-(async () => {
-  const rawReadme = await fs.readFile(path.join(__dirname, '..', 'README.md'))
+const { markdowner } = smhMarkdowner
+
+;(async () => {
+  const rawReadme = await fs.readFile(path.join(process.cwd(), '..', 'README.md'))
   const html = await markdowner(rawReadme)
 
   await fs.writeFile(`readme.json`, JSON.stringify(html, null, 2))
