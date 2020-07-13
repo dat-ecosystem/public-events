f656c595e3ff23f50f57cbd4b19c86928f547fab
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-13 20:25:43 +0900

    lint fix

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -3,17 +3,17 @@ const at = require('lodash.at')
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
 
-  const markdownIt = require("markdown-it")({
+  const markdownIt = require('markdown-it')({
     html: true,
     breaks: true,
     linkify: true
   })
-  const markdownItAttrs = require("markdown-it-attrs")
+  const markdownItAttrs = require('markdown-it-attrs')
 
   const mdIt = markdownIt.use(markdownItAttrs)
-  eleventyConfig.setLibrary("md", mdIt)
-  eleventyConfig.addFilter("md", input => markdownIt.render(input))
-  eleventyConfig.addFilter("find", (input, path, expected) => {
+  eleventyConfig.setLibrary('md', mdIt)
+  eleventyConfig.addFilter('md', input => markdownIt.render(input))
+  eleventyConfig.addFilter('find', (input, path, expected) => {
     for (entry of input) {
       if (at(entry, [path])[0] == expected) return entry
     }
