cd37a1e25329c7f6481d4db878c8847f8d872376
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-14 17:14:27 +0900

    Sorting people by name

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -1,5 +1,30 @@
 const at = require('lodash.at')
 
+function sort (input, property, desc) {
+  if (!Array.isArray(input)) {
+    input = Array.from(input)
+  }
+  if (!property) {
+    input = input.sort()
+  } else {
+    input = input.sort((a, b) => {
+      const aVal = a[property]
+      const bVal = b[property]
+      if (aVal > bVal) {
+        return 1
+      }
+      if (aVal < bVal) {
+        return -1
+      }
+      return 0
+    })
+  }
+  if (desc) {
+    input = input.reverse()
+  }
+  return input
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
 
@@ -13,6 +38,7 @@ module.exports = function (eleventyConfig) {
   const mdIt = markdownIt.use(markdownItAttrs)
   eleventyConfig.setLibrary('md', mdIt)
   eleventyConfig.addFilter('md', input => markdownIt.render(input))
+  eleventyConfig.addFilter('sort', sort)
   eleventyConfig.addFilter('find', (input, path, expected) => {
     for (entry of input) {
       if (at(entry, [path])[0] == expected) return entry
