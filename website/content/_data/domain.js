b622b83346f1e30b6296f2018c681028e465a7ed
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-22 14:47:22 +0900

    Fixing domain lookup (preventing images for the time beeing)

--- website/content/_data/domain.js
+++ website/content/_data/domain.js
@@ -1,10 +1,13 @@
 module.exports = () => {
-  let url = process.env.DEPLOY_DEPLOY_PRIME_URL || process.env.URL || 'http://localhost:8080'
+  let url = process.env.DEPLOY_PRIME_URL || process.env.URL || 'http://localhost:8080'
+  /*
+  Maybe doesn't work as expected?
   let image = process.env.NETLIFY_IMAGES_CDN_DOMAIN
   if (image) {
     image = `https://${image}`
   } else {
     image = url
   }
-  return { url, image }
+  */
+  return { url, image: url }
 }
