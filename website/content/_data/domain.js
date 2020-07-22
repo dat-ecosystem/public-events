e78cf0c9cd488102d06cc73799a809a92ddad8d8
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-22 14:42:30 +0900

    Fixing things :)

--- website/content/_data/domain.js
+++ website/content/_data/domain.js
@@ -1,6 +1,10 @@
 module.exports = () => {
-  return {
-    url: process.env.DEPLOY_DEPLOY_PRIME_URL || 'http://localhost:8080',
-    image: process.env.NETLIFY_IMAGES_CDN_DOMAIN || process.env.DEPLOY_DEPLOY_PRIME_URL  || 'http://localhost:8080'
+  let url = process.env.DEPLOY_DEPLOY_PRIME_URL || process.env.URL || 'http://localhost:8080'
+  let image = process.env.NETLIFY_IMAGES_CDN_DOMAIN
+  if (image) {
+    image = `https://${image}`
+  } else {
+    image = url
   }
+  return { url, image }
 }
