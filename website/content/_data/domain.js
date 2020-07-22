84d43d5315490180768ddf8e80ff85137335b50a
Martin Heidegger <martin.heidegger@gmail.com>
2020-07-23 01:52:33 +0900

    Fixing domain.

--- website/content/_data/domain.js
+++ website/content/_data/domain.js
@@ -1,5 +1,8 @@
 module.exports = () => {
   let url = process.env.DEPLOY_PRIME_URL || process.env.URL || 'http://localhost:8080'
+  if (process.env.CONTEXT === 'production') {
+    url = 'https://events.dat.foundation'
+  }
   /*
   Maybe doesn't work as expected?
   let image = process.env.NETLIFY_IMAGES_CDN_DOMAIN
