97056f3868b08410445a50a65e892dc1d0b96bef
Santiago Saavedra <info@ssaavedra.eu>
2020-07-28 21:39:13 +0200

    Add relative times to the time slot of the talks
    Also update a bit of styling on the talks page so that it looks nicer
    
    Co-Authored-By: Martin Heidegger <martin@consento.org>

--- website/assets/2020-schedule.js
+++ website/assets/2020-schedule.js
@@ -12876,4 +12876,53 @@ var proposalTemporal = createCommonjsModule(function (module, exports) {
   exports.Temporal = Temporal;
 });
 
-console.log(proposalTemporal);
+const {
+  Temporal
+} = proposalTemporal;
+/**
+ * 
+ * @param {Temporal.Duration} duration 
+ */
+
+function temporal_duration_to_human(duration) {
+  window.last_duration = duration;
+  const plural_day = duration.days != 1 ? "s" : "";
+  const plural_hour = duration.hour != 1 ? "s" : "";
+  const plural_minute = duration.minute != 1 ? "s" : "";
+  return `${!!duration.days ? `${duration.days} day${plural_day}, ` : ""}${!!duration.hours ? `${duration.hours} hour${plural_hour} and ` : ""}${duration.minutes} minute${plural_minute}`;
+}
+
+function add_relative_time(domNode) {
+  const start = Temporal.Absolute.from(domNode.dataset.start);
+  const end = Temporal.Absolute.from(domNode.dataset.end);
+  const now = Temporal.now.absolute();
+
+  if (Temporal.Absolute.compare(now, start) < 0) {
+    // Hasn't started yet
+    const diff = start.difference(now, {
+      largestUnit: 'days'
+    });
+    domNode.innerHTML = `in ${temporal_duration_to_human(diff)}`;
+  } else if (Temporal.Absolute.compare(now, end) < 0) {
+    // Has started already but hasn't ended
+    const diff = now.difference(start, {
+      largestUnit: 'days'
+    });
+    domNode.innerHTML = `live now, started ${temporal_duration_to_human(diff)} ago`;
+  } else {
+    // Has ended
+    const diff = now.difference(end, {
+      largestUnit: 'days'
+    });
+    domNode.innerHTML = `ended ${temporal_duration_to_human(diff)} ago`;
+  }
+}
+
+function update_all_relative_times() {
+  Array.from(document.querySelectorAll(".relative-when")).forEach(node => {
+    add_relative_time(node);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", update_all_relative_times);
+setInterval(update_all_relative_times, 30000);
